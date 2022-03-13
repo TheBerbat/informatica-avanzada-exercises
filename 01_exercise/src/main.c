b5517c9ce1e87f15ae6131cd83d3032e7a8c582f
Rafael Martin <theverbat@gmail.com>
2022-03-13 13:53:09 +0100

    Finished 01_exercise

--- 01_exercise/src/main.c
+++ 01_exercise/src/main.c
@@ -1,4 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <triangular.h>
+
 int main()
 {
-    return 0;
+    unsigned int n, res;
+
+    printf("Introduce an interger number: ");
+    if ( fscanf(stdin, "%d", &n) != 1)
+    {
+        printf("Error introducing the number, please rerun the program\n");
+        return EXIT_FAILURE;
+    }
+
+    res = triguangular(n);
+    printf("The result is: %u\n", res);
+
+    return EXIT_SUCCESS;
 }
