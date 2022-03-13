ab8ca4764bea46e91a45b900dc995bbfe9445323
Rafael Martin <theverbat@gmail.com>
2022-03-13 14:00:31 +0100

    Moved function description to header file

--- 01_exercise/inc/triangular.h
+++ 01_exercise/inc/triangular.h
@@ -1,3 +1,9 @@
 #pragma once
 
+/**
+ * @brief Function that calculates a triangular number given a number.
+ * 
+ * @param n Given number
+ * @return Triangular number
+ */
 unsigned int triguangular(unsigned int n);
--- 01_exercise/src/triangular.c
+++ 01_exercise/src/triangular.c
@@ -1,11 +1,5 @@
 #include <triangular.h>
 
-/**
- * @brief Function that calculates a triangular number given a number.
- * 
- * @param n Given number
- * @return Triangular number
- */
 unsigned int triguangular(unsigned int n)
 {
     return n*(n+1)/2;
